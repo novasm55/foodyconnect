9683a44632ba457b811dbb3f6ff81ac6310d2483
alexanderscher <alexcscher@gmail.com>
2023-04-20 16:24:00 -0700

    fixed login and signup

--- public/js/userProfile.js
+++ public/js/userProfile.js
@@ -1,11 +1,11 @@
-const viewUserProfile = async () => {
-  const username = document.querySelector(".username");
+// const viewUserProfile = async () => {
+//   const username = document.querySelector(".username");
 
-  const response = await fetch("/user/:id", {
-    method: "GET",
-    body: JSON.stringify({ email, password }),
-    headers: { "Content-Type": "application/json" },
-  });
-};
+//   const response = await fetch("/user/:id", {
+//     method: "GET",
+//     body: JSON.stringify({ email, password }),
+//     headers: { "Content-Type": "application/json" },
+//   });
+// };
 
-document.addEventListener("click", viewUserProfile);
+// document.addEventListener("click", viewUserProfile);
